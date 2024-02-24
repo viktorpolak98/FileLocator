cd09bd9f0520e69e035e7c56d38eaec7337f3040
viktorpolak98 <61971346+viktorpolak98@users.noreply.github.com>
2024-02-24 14:30:32 +0100

    Added testsuite

---
+++ src/TestSuite/TestSearchIDatastucture.java
@@ -0,0 +1,13 @@
+package TestSuite;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestSearchIDatastucture {
+
+    @BeforeAll
+    public static void setup(){
+        //Setup
+    }
+}
