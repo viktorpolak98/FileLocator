d40229ef11a49976e418ee25f70cf6680fd29893
viktorpolak98 <vigge.polak98@gmail.com>
2024-03-01 10:05:11 +0100

    Modified Cilent and Server classes
    Added Timeout function in client

--- src/Server/Runner/Server.java
+++ src/Server/Runner/Server.java
@@ -1,5 +1,7 @@
 package Server.Runner;
 
+import Server.Model.RequestTypes;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,20 +9,16 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class Server {
-    private final Executor executor;
     private final ServerSocket socket;
 
     public Server(int port) throws IOException {
-        executor = Executors.newCachedThreadPool();
         socket = new ServerSocket(port);
     }
 
     public void run() throws IOException {
-
         while (!socket.isClosed()){
             Socket client = socket.accept();
             new Client(client);
         }
     }
-
 }
