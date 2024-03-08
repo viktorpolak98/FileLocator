7daf94482dfc31e5e3a1e43fc1800ca7a79270b2
viktorpolak98 <vigge.polak98@gmail.com>
2024-03-09 00:22:33 +0100

    Server modifications

--- src/Interfaces/IDataStructure.java
+++ src/Interfaces/IDataStructure.java
@@ -2,9 +2,9 @@ package Interfaces;
 
 import java.io.File;
 import java.util.Vector;
+import java.util.concurrent.Future;
 
 public interface IDataStructure {
     void build(File rootFile, BuildingCallback callback);
-
     Vector<File> search(String searchQuery, FileFoundCallback fileFoundCallback);
 }
--- src/Server/Runner/Client.java
+++ src/Server/Runner/Client.java
@@ -3,9 +3,10 @@ package Server.Runner;
 import Interfaces.FileFoundCallback;
 import Interfaces.IDataStructure;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.StringTokenizer;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.Vector;
@@ -14,20 +15,43 @@ public class Client implements Runnable, FileFoundCallback {
     private final Socket socket;
     private IDataStructure structure;
     private final Timer timer = new Timer();
+    private BufferedReader reader;
+    private BufferedWriter writer;
+    private final String clientAddress;
+    private final Server server;
+    private final long TIME_OUT = 500_000;
 
-    public Client(Socket socket){
+    public Client(Socket socket, String clientAddress, Server server){
         this.socket = socket;
+        this.clientAddress = clientAddress;
+        this.server = server;
+
     }
 
     public void setStructure(IDataStructure structure) {
         this.structure = structure;
     }
 
+    public void handleRequest(){
+        try {
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            writer = new BufferedWriter(new PrintWriter(socket.getOutputStream(), true));
+
+            String input = reader.readLine();
+            StringTokenizer parse = new StringTokenizer(input);
+
+            String method = parse.nextToken().toUpperCase();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        restartTimer(TIME_OUT);
+    }
+
     @Override
     public void run() {
-        while(socket.isConnected()){
-            //do stuff
-        }
+        startTimer(TIME_OUT);
     }
 
     public Vector<File> search(String searchQuery){
@@ -54,6 +78,7 @@ public class Client implements Runnable, FileFoundCallback {
             public void run() {
                 try {
                     socket.close();
+                    server.removeClient(clientAddress);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
