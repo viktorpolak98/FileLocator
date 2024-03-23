09d6a5641a9d4a0f546b5bb219219f559ccc7d18
viktorpolak98 <vigge.polak98@gmail.com>
2024-03-23 13:10:36 +0100

    Added bad request response

--- src/Server/Controller/RequestHandler.java
+++ src/Server/Controller/RequestHandler.java
@@ -1,27 +1,42 @@
 package Server.Controller;
 
+import Interfaces.FileFoundCallback;
 import Interfaces.IDataStructure;
 import Server.Model.RequestTypes;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Optional;
+import java.util.StringTokenizer;
 
 public class RequestHandler {
     private final BufferedWriter writer;
     private final BufferedReader reader;
-    private final IDataStructure structure;
+    private IDataStructure structure;
 
-    public RequestHandler(BufferedWriter writer, BufferedReader reader, IDataStructure structure) {
+    public RequestHandler(BufferedWriter writer, BufferedReader reader) {
         this.writer = writer;
         this.reader = reader;
-        this.structure = structure;
     }
 
-    public static boolean handleRequest(Optional<RequestTypes> type){
+    public boolean handleRequest() throws IOException {
+        String input = reader.readLine();
+        StringTokenizer parse = new StringTokenizer(input);
 
+        String method = parse.nextToken().toUpperCase();
+
+        Optional<RequestTypes> type = RequestTypes.getRequestType(method);
+
+        if(type.isPresent()){
+            writer.write("HTTP/1.1 404 Bad request");
+            return false;
+        }
 
         return true;
     }
+
 
 }
