47d902c3dd2e2cb8abb30325a809f8a926af3d7f
viktorpolak98 <vigge.polak98@gmail.com>
2024-11-09 10:12:01 +0100

    Remove old UI

--- src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import Controller.MainController;
-import Controller.StartupController;
-
-public class Main {
-    public static void main(String[] args) {
-        new StartupController(5);
-    }
-}
