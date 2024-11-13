015ba5ddc985c57492797d0f7fcdcb9c52b33463
viktorpolak98 <vigge.polak98@gmail.com>
2024-11-13 20:36:31 +0100

    Finish implemetation of onSelectButtonAction

--- DesktopView/src/main/java/Controller/MainViewController.java
+++ DesktopView/src/main/java/Controller/MainViewController.java
@@ -1,5 +1,11 @@
 package Controller;
 
-public class MainViewController {
+import LogicInterfaces.IDataStructure;
 
+public class MainViewController {
+    private IDataStructure dataStructure;
+
+    public void setDataStructure(IDataStructure dataStructure) {
+        this.dataStructure = dataStructure;
+    }
 }
--- DesktopView/src/main/java/Controller/StructureChooserController.java
+++ DesktopView/src/main/java/Controller/StructureChooserController.java
@@ -1,9 +1,12 @@
 package Controller;
 
+import LogicFactories.DataStructureFactory;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,9 +15,20 @@ public class StructureChooserController {
     private Stage stage;
     private Application application;
     @FXML
+    ToggleGroup structureChooserGroup;
+
+    @FXML
     public void onSelectButtonAction() throws IOException {
+        RadioButton rBtn = (RadioButton)structureChooserGroup.getSelectedToggle();
+        String selection = rBtn.getText();
+        selection = selection.substring(0, selection.indexOf(" ")).toLowerCase();
+
         FXMLLoader loader = new FXMLLoader(application.getClass().getResource("main-view.fxml"));
         Scene scene = new Scene(loader.load(), 1000, 600);
+
+        MainViewController controller = loader.getController();
+        controller.setDataStructure(DataStructureFactory.buildEmptyStructure(selection));
+
         stage.setTitle("FileLocator");
         stage.setScene(scene);
     }
