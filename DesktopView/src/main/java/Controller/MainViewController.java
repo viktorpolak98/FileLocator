6a3f7457558ebc2677b6867ed0cef02be22ec35c
viktorpolak98 <vigge.polak98@gmail.com>
2024-11-14 18:32:57 +0100

    Add top part of main view

--- DesktopView/src/main/java/Controller/MainViewController.java
+++ DesktopView/src/main/java/Controller/MainViewController.java
@@ -1,11 +1,22 @@
 package Controller;
 
 import LogicInterfaces.IDataStructure;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class MainViewController {
     private IDataStructure dataStructure;
+    @FXML
+    TextField fileSearchField;
 
     public void setDataStructure(IDataStructure dataStructure) {
         this.dataStructure = dataStructure;
+    }
+
+    public void onRootSelectionAction(ActionEvent actionEvent) {
+    }
+
+    public void onSearchFileAction(ActionEvent actionEvent) {
     }
 }
