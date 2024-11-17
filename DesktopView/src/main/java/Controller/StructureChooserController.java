ee3c6c4be392742a39974b514f044f293b343ed2
viktorpolak98 <vigge.polak98@gmail.com>
2024-11-17 19:29:59 +0100

    Add error handling

--- DesktopView/src/main/java/Controller/StructureChooserController.java
+++ DesktopView/src/main/java/Controller/StructureChooserController.java
@@ -4,9 +4,16 @@ import LogicFactories.DataStructureFactory;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +27,23 @@ public class StructureChooserController {
     @FXML
     public void onSelectButtonAction() throws IOException {
         RadioButton rBtn = (RadioButton)structureChooserGroup.getSelectedToggle();
+        if (rBtn == null){
+            Stage popupStage = new Stage();
+            popupStage.initModality(Modality.APPLICATION_MODAL);
+            popupStage.initOwner(stage);
+            VBox box = new VBox();
+            box.setMinSize(200, 50);
+            Text text = new Text("Please make a selection");
+            text.setFont(Font.font(15));
+            box.getChildren().add(text);
+            box.setAlignment(Pos.CENTER);
+            Scene scene = new Scene(box);
+            popupStage.setScene(scene);
+            popupStage.show();
+
+            return;
+        }
+
         String selection = rBtn.getText();
         selection = selection.substring(0, selection.indexOf(" ")).toLowerCase();
 
